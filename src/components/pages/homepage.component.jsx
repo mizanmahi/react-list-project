9d5fdaee29bc05350dc646b40b9ff08cb50872c6
mizanmahi <mizanmahi24@gmail.com>
2021-05-09 03:52:01 +0600

    second commit

--- src/components/pages/homepage.component.jsx
+++ src/components/pages/homepage.component.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import "./homepage.style.css";
+
 const HomePage = (props) => {
   const [users, setUsers] = useState(null);
 
@@ -10,23 +12,38 @@ const HomePage = (props) => {
     async function fetchUsers(url) {
       const usersResponse = await axios.get(url);
       const usersData = await usersResponse.data;
-      setUsers(usersData);
+      return usersData;
     }
-    fetchUsers(URL);
-    console.log(users);
-  },[URL]);
+    fetchUsers(URL).then((d) => {
+      setUsers(d);
+      console.log(d.data.rows);
+    });
+  }, [URL]);
 
   return (
     <div className="homepage">
-      <table>
+      <table className="list-table">
         <tbody>
-          <tr>
+          <tr className="list-table__row">
             {users
               ? Object.keys(users.data.headers[0])
                   .map((key) => users.data.headers[0][key])
+                  .filter((th) => th.hidden === false)
                   .map((th) => <th key={th.title}>{th.title}</th>)
               : null}
           </tr>
+          {users
+            ? users.data.rows.map((row) => (
+                <tr key={row.id}>
+                  {Object.keys(row)
+                    .map((key) => row[key])
+                    .filter((td, i) => i <= 3)
+                    .map((td, i) => (
+                      <td key={i}>{td}</td>
+                    ))}
+                </tr>
+              ))
+            : null}
         </tbody>
       </table>
     </div>
